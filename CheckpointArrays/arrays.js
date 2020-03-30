73717150ab58827be79a5ee2f882ce4ebd7fd39e
Jrez300 <Joseperez940@live.com>
2020-03-30 11:17:52 -0400

    arrays

---
+++ CheckpointArrays/arrays.js
@@ -0,0 +1,32 @@
+// Three Array Methods
+//  .Push()
+//  .Indexof()
+//  .Pop()
+
+var drinks = []
+drinks.push("water")
+drinks.push("coffee")
+drinks.push("tea")
+
+console.log(drinks)
+
+var foods = []
+drinks.push("apple")
+drinks.push("sandwich")
+drinks.push("chips")
+
+console.log(foods)
+
+var meals = [
+    "water",
+    "coffee",
+    "tea",
+    "apple",
+    "sandwich",
+    "chips"
+]
+
+var reverseMeal = meals.reverse();
+
+
+console.log(reverseMeal)
